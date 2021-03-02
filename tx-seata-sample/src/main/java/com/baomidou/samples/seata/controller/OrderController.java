d1907a41fb2588dfad4468a100f4799abf75bb35
baopeng <baopeng@nuc>
2021-03-02 21:45:29 +0800

    seata update for ubuntu18

--- tx-seata-sample/src/main/java/com/baomidou/samples/seata/controller/OrderController.java
+++ tx-seata-sample/src/main/java/com/baomidou/samples/seata/controller/OrderController.java
@@ -5,10 +5,7 @@ import com.baomidou.samples.seata.service.OrderService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
@@ -23,7 +20,7 @@ public class OrderController {
         return "下单成功";
     }
 
-    @PostMapping("/test1")
+    @GetMapping("/test1")
     @ApiOperation("测试商品库存不足-异常回滚")
     public String test1() {
         //商品单价10元，库存20个,用户余额50元，模拟一次性购买22个。 期望异常回滚
@@ -31,7 +28,7 @@ public class OrderController {
         return "下单成功";
     }
 
-    @PostMapping("/test2")
+    @GetMapping("/test2")
     @ApiOperation("测试用户账户余额不足-异常回滚")
     public String test2() {
         //商品单价10元，库存20个，用户余额50元，模拟一次性购买6个。 期望异常回滚
